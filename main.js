1122f300eeae14d5892db7c0a04ac6724c9ed691
aminbasiran <work.aminbasiran@gmail.com>
2023-09-22 00:54:55 +0800

    fix toggle classlist

--- main.js
+++ main.js
@@ -1,10 +1,9 @@
 const hamburger = document.querySelector(".header__menu")
 const header = document.querySelector(".header")
-const HR = document.querySelector(".header__right")
+const header1 = document.querySelector(".header__right")
 
 
 hamburger.addEventListener("click",()=>{
-    header.style.height = "100px";
-    header.style.width = "100%";
-    HR.style.display = "flex";
+    header.classList.toggle("active")
+    header1.classList.toggle("active")
 })
