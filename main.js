a5a97c8e67667b8240a8748205b95e84154d6179
aminbasiran <work.aminbasiran@gmail.com>
2023-09-27 01:13:03 +0800

    completed until profile card. checkpoint in case profile card fails

--- main.js
+++ main.js
@@ -3,6 +3,24 @@ const header = document.querySelector(".header")
 const header1 = document.querySelector(".header__right")
 
 
+
+
+// function checkScreenWidth() {
+//     const screenWidth = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
+    
+//     if (screenWidth <= 769) {
+//         const divToRemove = document.querySelector('.main__right');
+//         if (divToRemove) {
+//             divToRemove.remove(); // Remove the div if it exists
+//         }
+//     }
+// }
+
+// // Check the screen width when the page loads and when it's resized
+// window.addEventListener('load', checkScreenWidth);
+// window.addEventListener('resize', checkScreenWidth);
+
+
 hamburger.addEventListener("click",()=>{
     header.classList.toggle("active")
     header1.classList.toggle("active")
